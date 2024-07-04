08b8fa0fbd041e5d1bbd5501b514298a3ef299cd
Punnatat Ngirnngam <punnatat_ng@cmu.ac.th>
2024-07-05 00:04:04 +0700

    let me try

--- q1.ts
+++ q1.ts
@@ -1,15 +1,27 @@
 // define interface for Hero and Shop objects
+//ปัณณทัต เงินงาม 660610775
+
 interface Hero {
   /* Your code here */
+  items:string[];
+  gold:number;
 }
 
 interface Shop {
   /* Your code here */
+  item:string;
+  price:number;
 }
 
 // assign interface/type to the function definition properly
-function buyItem(hero, shop) {
+function buyItem(hero:Hero, shop:Shop):Hero{
   /* Your code here */
+  if(hero.gold>=shop.price){
+    hero.items.push(shop.item);
+    hero.gold=hero.gold-shop.price;
+    return hero;
+  }
+  else return hero;
 }
 
 //Test cases : assign proper type/interface to all objects
--- q2.ts
+++ q2.ts
@@ -1,9 +1,15 @@
 // define interface for Student object
+interface Student{
+  name :string;
+  score:number;
+}
 /* Your code here */
-
+//ปัณณทัต เงินงาม 660610775
 // assign interface/type to the function definition properly
-function findTopNames(students) {
+function findTopNames(students:Student[]){
   /* Your code here */
+  let output = students.filter((students)=>students.score>8).map((students)=>students.name);
+  return output;
 }
 
 // assign interface/type to the student1 object properly
